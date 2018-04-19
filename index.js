9ad2bfbc862210baf71ecd7d0cb9ec397777451e
CreativeBuilds <gaminggonecreative@gmail.com>
2018-04-19 12:19:03 -0400

    Started work on swapping over to a server based system

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const url = require('url');
+const io_client = require('socket.io-client');
 
 let win
-  
-function createWindow () {
-    // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600})
 
-    win.maximize();
+/* Make this file connect to the server and not index.html */
+
+/*
+ * 1. User connects to the server
+ *  - If error, show that the server is down for maitence
+ *  - If no error continue
+ * 2. Show login screen (for now we'll just log in with google)
+ * 3. Send the users information from the server to this client
+ * 4. Load the game with the new client information
+ */
+let socket;
+let timer;
+  
+function createWindow (fileName) {
+    // Create the browser window.
+
+    win = new BrowserWindow({width: 800, height: 600});
+    
+
+    //win.maximize();
 
     // and load the index.html of the app.
     win.loadURL(url.format({
-        pathname: path.join(__dirname, 'html/index.html'),
+        pathname: path.join(__dirname, `html/${fileName}.html`),
         protocol: 'file:',
         slashes: true
     }))
@@ -20,10 +36,29 @@ function createWindow () {
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
-        win = null
     })
 }
-  app.on('ready', createWindow)
+
+app.on('ready', function(){
+  socket = io_client('http://localhost:3000');
+
+  socket.on('connect', function(){
+    console.log('Connected to server!');
+    createWindow('index');
+    if(timer){
+      clearTimeout(timer);
+    }
+  })
+
+  socket.on('connect_error', function(){
+    if(!timer){
+      timer = setTimeout(function(){
+        console.log('Failed to connect to the server');
+        createWindow('error');
+      },15000)
+    }
+  })
+})
   
   // Quit when all windows are closed.
   app.on('window-all-closed', () => {
@@ -38,7 +73,7 @@ function createWindow () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (win === null) {
-      createWindow()
+      createWindow('index')
     }
   })
   
